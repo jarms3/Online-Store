19f982ac03bd2ef2dfe1f592ce1682e4d6fe31f3
Josh Armstrong <jarms3@uwo.ca>
2018-12-06 02:06:52 +0000

    can add items to collection

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ var db = firebase.database();
 var ref = db.ref("login");
 var pef = db.ref("products");
 var cartRef = db.ref("cart");
+var cRef  = db.ref("collections");
 
 
 
@@ -66,7 +67,7 @@ router.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin','*');
     res.header("Access-Control-Allow-Headers", "Origin, XRequested-With,Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'POST, PATCH, GET, PUT, DELETE, OPTIONS');
-    console.log('Something is happening.');
+    //console.log('Something is happening.');
     next(); // make sure we go to the next routes and don't stop here
 });
 
@@ -74,6 +75,7 @@ router.route('/auth/:user/:pass')
 
 .get(function(req, res){
     var userRef = ref.child("users");
+    var newCart = true;
     userRef.once('value', function (snap) { // Keep the local user object synced with the Firebase userRef
         if (snap.val () == null) {
             
@@ -85,6 +87,7 @@ router.route('/auth/:user/:pass')
                 username: data.val ().username,
                 password: data.val ().password,
                 verified: data.val().verified, 
+                permission: data.val().permission,
                 status: data.val().status
               }
               list.push (item);
@@ -92,10 +95,17 @@ router.route('/auth/:user/:pass')
         }
         for(var j = 0; j < list.length; j++){
             
-            if(req.params.user == list[j].username && list[j].verified == true && list[j].status == "activated"){
+            if(req.params.user == list[j].username){
                 if(passwordHash.verify(req.params.pass, list[j].password)){
-                    if(req.params.user == "admin"){
-                        res.json("ADMIN_Sucess")
+                    if(!list[j].verified){
+                        res.json({succ: "Not verified"});
+                        return;
+                    } else if(list[j].status == "deactivated"){
+                        res.json({succ: "Not activated"});
+                        return;
+                    }
+                    else if(req.params.user == "store manager"){
+                        res.json({succ: "ADMIN_Sucess", permission: list[j].permission})
                         return;
                     }
                     else{
@@ -105,19 +115,24 @@ router.route('/auth/:user/:pass')
                                 if(item.val().user == req.params.user){
                                     cartRef.child(item.key + "/items").remove();
                                     cartRef.child(item.key).update({total: 0.00});
-                                }else
-                                    cartRef.push().set({user: req.params.user, total: 0.00});
+                                    newCart = false;
+                                }
+                                    
                             });
+                            
+                            if(newCart)
+                                cartRef.push().set({user: req.params.user, total: 0.00});
                         });
                             
                         
-                        res.json("Sucess");
+                        res.json({succ: "Sucess", permission: list[j].permission});
                         return;
                     }
                 }
                 else{
-                    res.json("Fail");
-                    return;
+                   res.json({succ: "Fail"});
+                   return; 
+                    
                 }
             } 
                 
@@ -165,7 +180,9 @@ router.route('/newuser')
         newUser.push().set({
                 username: req.body.user,
                 password: hashedPassword,
-                verified: false
+                verified: false,
+                status: "activated",
+                permission: false
         });
         
         userRef.once('value', function (snaps) { // Keep the local user object synced with the Firebase userRef
@@ -275,19 +292,84 @@ router.route('/products/:product_id')
 });
 
 
+router.route('/users/:name')
+
+.get(function(req, res){
+    ref.child("users").once('value', function(sap){
+        sap.forEach(function(data){
+            if(data.val().username == req.params.name){
+                res.json({name: [data.val().username], status: data.val().status, check: data.val().permission});
+            }
+        });
+    });
+    
+})
+
+.put(function(req, res){
+    ref.child("users").once('value', function(sap){
+        sap.forEach(function(data){
+            if(data.val().username == req.params.name){
+                ref.child("users/" + data.key).update({status: req.body.act})
+                res.json("Updated");
+            }
+        });
+    });
+})
+
+.post(function(req, res){
+   ref.child("users").once('value', function(sap){
+        sap.forEach(function(data){
+            if(data.val().username == req.params.name){
+                ref.child("users/" + data.key).update({permission: req.body.perm})
+                res.json("Updated");
+            }
+        });
+    }); 
+});
 
 
+router.route('/cart/:username')
 
-router.route('/cart')
+.get(function(req, res){
+    var username = req.params.username;
+    var key;
+    var total;
+    cartRef.once('value', function(snap){
+        snap.forEach(function(data) {
+            if(username == data.val().user){
+                key = data.key;
+                var list = new Array ();
+                total = data.val().total;
+                var listRef = cartRef.child(key + "/items"); 
+                listRef.once('value', function(cap){
+                    cap.forEach(function(item){
+                        list.push(item.val());
+                    });
+                    
+                    var cart = {
+                        _id: key,
+                        names: list,
+                        total: total
+                    }
+                    
+                    res.json(cart);
+                    
+                });
+                
+               
+            }
+        });
+       
+    });
+})
 
 .post(function(req, res){
     var price = req.body.cost;
     var amount = req.body.quantity;
     var tax = req.body.taxrate;
-    var username = req.body.username;
     var prodKey;
     var key;
-    var check;
+    var check = false;
     var total = (price * amount)*(1+tax);
     pef.once('value', function(prod){
         prod.forEach(function(data){
@@ -300,7 +382,7 @@ router.route('/cart')
     })
     cartRef.once('value', function(snap){
         snap.forEach(function(data) {
-            if(username == data.val().user){
+            if(req.params.username == data.val().user){
                 key = data.key;
                 var s = cartRef.child(key);
                 var add = data.val().total;
@@ -316,7 +398,8 @@ router.route('/cart')
                     
                     if(!check){
                        itemref.push().set({
-                            name: req.body.item
+                            name: req.body.item,
+                            amount: amount
                         }); 
                     }
                     
@@ -332,41 +415,6 @@ router.route('/cart')
     
 });
 
-router.route('/cart/:username')
-
-.get(function(req, res){
-    console.log("yep");
-    var username = req.params.username;
-    var key;
-    var total;
-    cartRef.once('value', function(snap){
-        snap.forEach(function(data) {
-            if(username == data.val().user){
-                key = data.key;
-                var list = new Array ();
-                total = data.val().total;
-                
-                var listRef = cartRef.child(key + "/items"); 
-                listRef.once('value', function(cap){
-                    cap.forEach(function(item){
-                        list.push(item.val().name);
-                    })
-                    
-                    var cart = {
-                        _id: key,
-                        names: list,
-                        total: total
-                    }
-                    
-                     res.json(cart);
-                })
-                
-               
-            }
-        });
-       
-    });
-});
 
 router.route('/cart/buy/:cart_id')
 
@@ -374,6 +422,24 @@ router.route('/cart/buy/:cart_id')
     cartRef.child(req.params.cart_id + "/items").remove();
     cartRef.child(req.params.cart_id).update({total: 0.00})
     res.json("Thank you for your purchase");
+});
+
+router.route('/cart/cancel/:cart_id')
+
+.put(function(req, res){
+    pef.once('value', function(prod){
+        prod.forEach(function(data){
+            for(var j = 0; j < req.body.stuff.length; j++){
+                if(req.body.stuff[j].name == data.val().name){
+                   pef.child(data.key).update({quantity: data.val().quantity + req.body.stuff[j].amount}); 
+                }
+            }
+                
+        });
+    });
+    cartRef.child(req.params.cart_id + "/items").remove();
+    cartRef.child(req.params.cart_id).update({total: 0.00})
+    res.json("Cart cleared");
 });
 
 router.route('/comments/:name')
@@ -385,8 +451,8 @@ router.route('/comments/:name')
             if(req.params.name == dink.val().name){
                 pef.child(dink.key + "/comments").once('value', function(comm){
                     comm.forEach(function(data){
-                        
-                        comments.push(data.val().comment);
+                       console.log(data.val());
+                        comments.push(data.val());
                     });
                     res.json(comments);
                 });
@@ -401,7 +467,10 @@ router.route('/comments/:name')
         snap.forEach(function(dink){
             if(req.params.name == dink.val().name){
                 pef.child(dink.key + "/comments").push().set({
-                    comment: req.body.comment
+                    comment: req.body.comment,
+                    user: req.body.user,
+                    key: dink.key,
+                    visible: true
                 });
                 
                 res.json("Added Comment")
@@ -409,6 +478,43 @@ router.route('/comments/:name')
         });
     });
 });
+
+router.route('/collection/:user')
+
+.post(function(req, res){
+    var check = true;
+    cRef.once('value', function(snap){
+        snap.forEach(function(data){
+            if(data.val().user == req.params.user){
+                cRef.child(data.key + "/items").push().set({
+                    item: req.body.item,
+                    price: req.body.cost,
+                    amount: req.body.quantity
+                });
+                
+                check = false;
+            } 
+        });
+        
+        if(check){
+            cRef.push().set({
+                user: req.params.user,
+                view: "private",
+            });
+            cRef.once('value', function(snap){
+                snap.forEach(function(data){
+                    if(data.val().user == req.params.user){
+                        cRef.child(data.key + "/items").push().set({
+                            item: req.body.item,
+                            price: req.body.cost,
+                            amount: req.body.quantity
+                        });
+                    }
+                });
+            });
+        }
+    })
+})
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
